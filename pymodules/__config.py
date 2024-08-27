4b40e2db85180b5880f55c277b7a1615f24424a5
SurceBeats <subcero@gmail.com>
2024-08-27 14:11:07 +0200

    multiseed in __config.py

--- pymodules/__config.py
+++ pymodules/__config.py
@@ -2,6 +2,11 @@
 
 import hashlib
 
-seed = 42
+# seed = 42  # Just scifi yo
+# seed = 137  # Fine structure constant
+# seed = 2718281828  # Eulers constant (e) approx to 10
+# seed = 314159  # Mostly Pi
+seed = 16180339887  # Golden ratio approx
+
 version = "0.3.11"
 versionHash = hashlib.sha256(version.encode("utf-8")).hexdigest()
