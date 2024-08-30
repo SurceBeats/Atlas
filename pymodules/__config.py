0c6a01262096d8ebd4847d2c14d3cb4f0f71dadb
SurceBeats <subcero@gmail.com>
2024-08-30 13:33:48 +0200

    atlas.ini automatic generation if missing

--- pymodules/__config.py
+++ pymodules/__config.py
@@ -3,24 +3,7 @@
 import hashlib
 import configparser
 import os
-
-
-def create_default_config(filename="atlas.ini"):
-    config = configparser.ConfigParser()
-
-    config["Settings"] = {
-        "seed": "1.618033988749895",
-        "image_quality": "100",
-        "enable_cache": "True",
-        "cache_cleanup_time": "900",
-    }
-
-    with open(filename, "w") as configfile:
-        config.write(configfile)
-
-
-if not os.path.exists("atlas.ini"):
-    create_default_config()
+import time
 
 config = configparser.ConfigParser()
 config.read("atlas.ini")
@@ -32,3 +15,24 @@ cache_cleanup_time = int(config.get("Settings", "cache_cleanup_time"))
 
 version = "0.6.51"
 versionHash = hashlib.sha256(version.encode("utf-8")).hexdigest()
+
+
+def create_default_config(filename="atlas.ini"):
+    config = configparser.ConfigParser()
+
+    cosmic_origin_time = int(time.time())
+
+    config["Settings"] = {
+        "seed": "1.618033988749895",
+        "cosmic_origin_time": str(cosmic_origin_time),
+        "image_quality": "100",
+        "enable_cache": "True",
+        "cache_cleanup_time": "900",
+    }
+
+    with open(filename, "w") as configfile:
+        config.write(configfile)
+
+
+if not os.path.exists("atlas.ini"):
+    create_default_config()
